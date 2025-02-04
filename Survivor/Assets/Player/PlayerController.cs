c69862d5217b089dd8d88d7a3ec827a1bb062b00
WikosS <theo.darques@epitech.eu>
2025-02-04 13:29:41 +0100

    Added player camera and direction

--- Survivor/Assets/Player/PlayerController.cs
+++ Survivor/Assets/Player/PlayerController.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour, IEntity
 {
-    public float Health { get; set; }
+
+    public float Health { get; set; } = 100f;
 
     public void TakeDamage(float damage)
     {
@@ -16,12 +17,14 @@ public class PlayerController : MonoBehaviour, IEntity
     public float movementSpeed;
     float speedX, speedY;
     Rigidbody2D rb;
+    SpriteRenderer sr;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -29,6 +32,14 @@ public class PlayerController : MonoBehaviour, IEntity
     {
         speedX = Input.GetAxis("Horizontal");
         speedY = Input.GetAxis("Vertical");
+        if (speedX < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (speedX > 0)
+        {
+            sr.flipX = false;
+        }
         Vector2 mov = new Vector2(speedX, speedY);
         if (mov.magnitude > 1) {mov.Normalize();}
         rb.linearVelocity = mov * movementSpeed;
